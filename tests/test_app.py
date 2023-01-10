8185fabc0ad106e2ac525ff99c09ceabe9cc18a2
Vadim Gusak <gusakvd@gmail.com>
2023-01-10 12:19:03 +1000

    Add tests

---
+++ tests/test_app.py
@@ -0,0 +1,45 @@
+from form_checker.app import app
+
+
+app.config.update(
+    SECRET_KEY='test_secret',
+    TESTING=True
+)
+
+
+def test_form_post():
+    post_data = {
+        'created_at': "09.01.2023",
+        "user_email": "test@mail.ru",
+        "unknown_field": "test_text",
+        "user_phone": "+79146789281"
+    }
+    response = app.test_client().post('/get_form', json=post_data)
+
+    assert response.json == {'FormName': 'AllFields'}
+
+
+def test_form_post_one_field():
+    post_data = {
+        'created_at': "09.01.2023",
+        "lol": "test@mail.ru",
+        "kek": "test_text",
+        "zzzz": "+79146789281"
+    }
+    response = app.test_client().post('/get_form', json=post_data)
+
+    assert response.json == {'FormName': 'OnlyDate'}
+
+
+def test_form_post_unknown_fields():
+    post_data = {
+        'blah': "09.01.2023",
+        "lol": "test@mail.ru",
+        "kek": "test_text",
+        "zzzz": "+79146789281"
+    }
+    response = app.test_client().post('/get_form', json=post_data)
+
+    assert response.json == {
+        'blah': 'date', 'kek': 'text', 'lol': 'email', 'zzzz': 'phone'
+    }
